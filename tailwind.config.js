c1476e90b97a05de06bb67d68ed85c617375f6eb
yeonsuBeak <soothbugger@gmail.com>
2022-11-29 20:50:03 +0900

    feat: html 문서 및 tailwind css 초기 설정 완료

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        black: "#222222",
+        white: "#ffffff",
+        blue: "#1755DA",
+        gray: "#F4F4F4",
+      },
+      fontFamily: {
+        body: ["Noto Sans KR"],
+      },
+    },
+  },
+};
